ac9c796934a854aa5f047f7aa6adfb1d3c2a19e9
Ali Çavuşlu <cavuslumehmet@icloud.com>
2023-05-04 23:45:34 +0300

    Initial Commit

---
+++ StocksEye/StocksEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksEyeApp.swift
+//  StocksEye
+//
+//  Created by Ali on 4.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct StocksEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
